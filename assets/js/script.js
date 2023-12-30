f8f994c8279878712ed6f91e89a0cec6bdaf54b9
CS89 <chrismacaroline@gmail.com>
2023-12-30 23:48:25 +0000

    code now displays first question and choices- need to make button clickable and timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,56 +2,70 @@
 const quizQuestions = [
 
   {
-    questionOne : "How many sets of teeth does a human being have?",
-    answersA : "One",
-    answersB : "Two",
-    answersC : "Three",
-    answersD : "Four"
+    question : "How many sets of teeth does a human being have?",
+    answers : ["One","Two","Three","Four"],
+    // correctAnswer : quizQuestions.answers[1],
   },
 
   {
-    questionTwo : "What is the total number of teeth in a primary dentition?",
-    answersA : "20",
-    answersB : "32",
-    answersC : "34",
-    answersD : "24"
+    question : "What is the total number of teeth in a primary dentition?",
+    answers : ["20", "32","34","24"],
+    //  correctAnswer : quizQuestions.answers[0],
+    
   },
 
-  {questionThree : "When does the first lower permanent molar erupt?",
-    answersA : "5-6 years",
-    answersB : "7-8 months",
-    answersC : "5-6 months",
-    answersD : "6-7 years"
+  {question : "When does the first lower permanent molar tooth erupt?",
+    answersA : ["5-6 years","7-8 months","5-6 months","6-7 years"],
+    // correctAnswer : quizQuestions.answers[0],
   },
 
   {
-    questionFour : "How many roots does a upper central incisor have?",
-    answersA : "One",
-    answersB : "Two",
-    answersC : "Three",
-    answersD : "Four"
+    question : "How many roots does an upper central incisor tooth have?",
+    answers : ["Two","One","Four","Three"],
+    // correctAnswer : quizQuestions.answers[1],
+    
+
   },
 
   {
-    questionFive : "How many teeth does a secondary dentition contain?",
-    answersA : "20",
-    answersB : "32",
-    answersC : "28",
-    answersD : "34"
+    question: "How many teeth does the secondary dentition contain?",
+    answers : ["20", "34","30","32"],
+    // correctAnswer : quizQuestions.answers[3],
   }
 ]
+
+// start button linked to script.js and landind page contents display set to none
+
   const start = document.querySelector("#start")
   start.addEventListener("click", quizStart)
 function quizStart () {
-  const wrapper = document.querySelector(".wrapper")
-  wrapper.style.display = "none";
-  // for (let i = 0; i < quizQuestions.length; i++) {
-  //   // console.log(quizQuestions[0].questionOne)
-  //   startQuiz.textContent = quizQuestions[0].questionOne
-    
-  // }
-
+  const startScreen = document.querySelector("#start-screen")
+  startScreen.style.display = "none";
+  quizDisplay()
 }
+ function quizDisplay(){
+  const questions = document.querySelector("#questions")
+  questions.style.display = "block";
+
+  for (let i = 0; i < quizQuestions.length; i++) {
+    const startQuiz = document.querySelector("#question-title")
+    startQuiz.textContent = quizQuestions[i].question
+    quizChoices()
+    return
+    }
+}
+
+function quizChoices (){
+  for (let i=0; i<4; i++){
+  const startChoices = document.querySelector(".choices")
+    const button = document.createElement("button")
+
+    button.textContent = quizQuestions[0].answers[i]
+    // button.className = "button"
+    startChoices.appendChild(button)
+}
+}
+// console.log("button")
 
 
 
