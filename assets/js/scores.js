2b2f9b68f26a4b8f37539fca222e843b113b9ed4
CS89 <chrismacaroline@gmail.com>
2024-01-02 16:13:00 +0000

    highscores are being displayed now- need to sort it from high to low

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,38 +1,49 @@
-// Storing user initials and displaying high scores
-
-// const submitBtn = document.querySelector("#submit")
-// const finalScore = document.querySelector("#final-score");
-
-// submitBtn.addEventListener("click", function(event){
-//   event.preventDefault();
-
-//   let initials = document.querySelector("#initials").value
-//   let final_score = finalScore.textContent
-//   localStorage.setItem("initials",JSON.stringify(initials))
-//   localStorage.setItem("final-score", JSON.stringify(final_score))
-//   window.location.href = "highscores.html";
-//   }
-// )
-
 function displayHighscores(){
 
   
   const highScoreslist = document.querySelector("#highscores")
-  const userHighscores = JSON.parse(localStorage.getItem("final-score"))
+  let highScore = JSON.parse(localStorage.getItem("highScore"))|| [];
+// for(let i=0; i < highScore.length; i++){
+
+// console.log(highScoreslist)
+highScoreslist.innerHTML = "";
+const userHighscores = JSON.parse(localStorage.getItem("final_score"))
   const userInitials = JSON.parse(localStorage.getItem("initials"))
-const initials = document.createElement("li")
-highScoreslist.appendChild(initials)
+// const initials = document.createElement("li")
+
   
   // initials.textContent = userInitials
-  initials.textContent = userInitials +" - "+ userHighscores;
+ 
 
+highScore.push({"initials" : userInitials, "final_score" : userHighscores})
 
+localStorage.setItem("highScore", JSON.stringify(highScore))
+
+for (let i =0; i < highScore.length; i++){
+  const displayscore = highScore[i];
+  let initials = document.createElement("li") 
+  initials.textContent = displayscore.initials + " - " + displayscore.final_score;
+  highScoreslist.appendChild(initials)
 }
+
+console.log(highScore)
+console.log(userHighscores)
+}
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
   displayHighscores();
-}
-)
+});
+
+
+// const clearScore = document.querySelector("#clear")
+
+// clearScore.addEventListener("click", clear())
+
+// function clear(){
+//  localStorage.clear()
+// }
+
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -160,28 +160,28 @@ submitBtn.addEventListener("click", function(event){
   let initials = document.querySelector("#initials").value
   let final_score = finalScore.textContent
   localStorage.setItem("initials",JSON.stringify(initials))
-  localStorage.setItem("final-score", JSON.stringify(final_score))
+  localStorage.setItem("final_score", JSON.stringify(final_score))
   window.location.href = "highscores.html";
   }
 )
 
-function displayHighscores(){
+// function displayHighscores(){
 
   
-  const highScoreslist = document.querySelector("#highscores")
-  const userHighscores = JSON.parselocalStorage.getItem("final-score")
-  const userInitials = JSON.parse(localStorage.getItem("initials"))
-const initials = document.createElement("li")
-highScores.appendChild(initials)
-  highScores.textContent = userHighscores;
-  initials.textContent = userInitials
+//   const highScoreslist = document.querySelector("#highscores")
+//   const userHighscores = JSON.parselocalStorage.getItem("final-score")
+//   const userInitials = JSON.parse(localStorage.getItem("initials"))
+// const initials = document.createElement("li")
+// highScores.appendChild(initials)
+//   highScores.textContent = userHighscores;
+//   initials.textContent = userInitials
   
 
 
-}
+// }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  displayHighscores();
+  // displayHighscores();
 }
 )
 
